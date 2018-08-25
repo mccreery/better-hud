c3e41d72ef3b664840fea42dded7d4219fc01b9e
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2018-08-25 12:56:04 +0100

    Add unchanged column to normalizelang.py output

--- normalizelang.py
+++ normalizelang.py
@@ -4,7 +4,7 @@ import os, sys
 ROOT_DIR = "src/main/resources/assets/hud/lang"
 CANONICAL = "en_US.lang"
 
-def print_row(column_width, *cells):
+def print_row(*cells, column_width = 10):
     print(*(str(cell).ljust(column_width) for cell in cells))
 
 def get(line):
@@ -18,10 +18,10 @@ def get(line):
 with open(os.path.join(ROOT_DIR, CANONICAL)) as f:
     lines = f.readlines()
 
-print_row(12, "file", "", "translated", "untranslated", "unmapped")
+print_row("file", "", "changed", "unchanged", "missing", "extra")
 
 entries = sum(bool(get(l)) for l in lines)
-print_row(12, CANONICAL, "100%", entries, 0, 0)
+print_row(CANONICAL, "100%", 0, entries, 0, 0)
 
 for f in os.listdir(ROOT_DIR):
     if f.lower() == CANONICAL.lower(): continue
@@ -35,6 +35,7 @@ for f in os.listdir(ROOT_DIR):
     with open(os.path.join(ROOT_DIR, f), "w", encoding="utf-8") as f1:
         untranslated = 0
         translated = 0
+        identical = 0
 
         for line in lines:
             kv = get(line)
@@ -42,7 +43,11 @@ for f in os.listdir(ROOT_DIR):
                 key = kv[0]
 
                 if key in mapping:
-                    translated += 1
+                    if mapping[key] == kv[1]:
+                        identical += 1
+                    else:
+                        translated += 1
+
                     f1.write(key + "=" + mapping[key] + "\n")
                     del mapping[key]
                 else:
@@ -51,8 +56,8 @@ for f in os.listdir(ROOT_DIR):
             else:
                 f1.write(line)
 
-        score = round(translated / (untranslated + translated) * 100)
-        print_row(12, f, str(score) + "%", translated, untranslated, len(mapping))
+        score = round((translated + identical) / entries * 100)
+        print_row(f, str(score) + "%", translated, identical, untranslated, len(mapping))
 
         if mapping:
             f1.write("\n# Unmapped lines\n")
