394e8db6e88c039641f3f590796090404bb61c9b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2018-08-25 19:10:20 +0100

    Add positioning to experience bar

--- src/main/java/tk/nukeduck/hud/element/vanilla/Experience.java
+++ src/main/java/tk/nukeduck/hud/element/vanilla/Experience.java
@@ -1,25 +1,28 @@
 package tk.nukeduck.hud.element.vanilla;
 
-import static tk.nukeduck.hud.BetterHud.ICONS;
 import static tk.nukeduck.hud.BetterHud.MANAGER;
 import static tk.nukeduck.hud.BetterHud.MC;
 
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import tk.nukeduck.hud.element.settings.SettingBoolean;
+import tk.nukeduck.hud.element.settings.SettingPosition;
 import tk.nukeduck.hud.util.Bounds;
 import tk.nukeduck.hud.util.Colors;
 import tk.nukeduck.hud.util.Direction;
+import tk.nukeduck.hud.util.Direction.Options;
 import tk.nukeduck.hud.util.GlUtil;
 import tk.nukeduck.hud.util.Point;
 
 public class Experience extends OverrideElement {
 	private final SettingBoolean hideMount = new SettingBoolean("hideMount");
+	private final SettingPosition position = new SettingPosition("position", Options.BAR, Options.NORTH_SOUTH);
 
 	public Experience() {
 		super("experience");
-		hideMount.set(true);
+
+		settings.add(hideMount);
+		settings.add(position);
 	}
 
 	@Override
@@ -42,27 +45,25 @@ public class Experience extends OverrideElement {
 
 	@Override
 	protected Bounds render(Event event) {
-		MC.getTextureManager().bindTexture(ICONS);
-
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-		GlStateManager.disableBlend();
-
 		Bounds bgTexture = new Bounds(0, 64, 182, 5);
 		Bounds fgTexture = new Bounds(0, 69, 182, 5);
 
-		Bounds barBounds = MANAGER.position(Direction.SOUTH, new Bounds(bgTexture), false, 1);
+		Bounds barBounds = new Bounds(bgTexture);
+
+		if(!position.isCustom() && position.getDirection() == Direction.SOUTH) {
+			barBounds = MANAGER.position(Direction.SOUTH, barBounds, false, 1);
+		} else {
+			barBounds = position.applyTo(barBounds);
+		}
 		GlUtil.drawTexturedProgressBar(barBounds.getPosition(), bgTexture, fgTexture, MC.player.experience, Direction.EAST);
 
 		if(MC.player.experienceLevel > 0) {
 			String numberText = String.valueOf(MC.player.experienceLevel);
-			Point numberPosition = new Bounds(GlUtil.getStringSize(numberText)).anchor(barBounds, Direction.NORTH).getPosition().sub(0, 6);
+			Point numberPosition = new Bounds(GlUtil.getStringSize(numberText))
+				.anchor(barBounds.grow(6), position.getContentAlignment().mirrorRow()).getPosition();
 
 			GlUtil.drawBorderedString(numberText, numberPosition.getX(), numberPosition.getY(), Colors.fromRGB(128, 255, 32));
 		}
-
-		GlStateManager.enableBlend();
-		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
 		return barBounds;
 	}
 }
