f3c454805664ae21d10e17379ba9c84b4542ff3b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2018-09-27 22:09:10 +0100

    Move normalize script to function

--- normalizelang.py
+++ normalizelang.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import os, sys
+import os
 
 ROOT_DIR = "src/main/resources/assets/hud/lang"
 CANONICAL = "en_US.lang"
 
-def print_row(*cells, column_width = 10):
-    print(*(str(cell).ljust(column_width) for cell in cells))
+HEADER = ("file", "score", "changed", "unchanged", "missing", "extra")
+STORE_FROM = 1
 
 def get(line):
     line = line.strip()
@@ -15,53 +15,79 @@ def get(line):
     else:
         return None
 
-with open(os.path.join(ROOT_DIR, CANONICAL), encoding="utf-8") as f:
-    lines = f.readlines()
+def get_row(silent, *cells, column_width = 10):
+    if not silent:
+        print(" ".join(str(cell).ljust(column_width) for cell in cells))
 
-print_row("file", "", "changed", "unchanged", "missing", "extra")
+    return {HEADER[i]: cells[i] for i in range(STORE_FROM, min(len(cells), len(HEADER)))}
 
-entries = sum(bool(get(l)) for l in lines)
-print_row(CANONICAL, "100%", 0, entries, 0, 0)
+def get_data(silent = False):
+    data = {}
 
-for f in os.listdir(ROOT_DIR):
-    if f.lower() == CANONICAL.lower(): continue
+    with open(os.path.join(ROOT_DIR, CANONICAL), encoding="utf-8") as f:
+        lines = f.readlines()
 
-    mapping = {}
-    with open(os.path.join(ROOT_DIR, f), "r", encoding="utf-8") as f1:
-        for line in f1.readlines():
-            kv = get(line)
-            if kv: mapping[kv[0]] = kv[1]
+    get_row(silent, *HEADER)
 
-    with open(os.path.join(ROOT_DIR, f), "w", encoding="utf-8") as f1:
-        untranslated = 0
-        translated = 0
-        identical = 0
+    entries = sum(bool(get(l)) for l in lines)
+    data[CANONICAL] = get_row(silent, CANONICAL, "100%", 0, entries, 0, 0)
 
-        for line in lines:
-            kv = get(line)
-            if kv:
-                key = kv[0]
+    scores = []
 
-                if key in mapping:
-                    if mapping[key] == kv[1]:
-                        identical += 1
+    for f in os.listdir(ROOT_DIR):
+        if f.lower() == CANONICAL.lower(): continue
+
+        mapping = {}
+        with open(os.path.join(ROOT_DIR, f), "r", encoding="utf-8") as f1:
+            for line in f1.readlines():
+                kv = get(line)
+                if kv: mapping[kv[0]] = kv[1]
+
+        with open(os.path.join(ROOT_DIR, f), "w", encoding="utf-8") as f1:
+            untranslated = 0
+            translated = 0
+            identical = 0
+
+            for line in lines:
+                kv = get(line)
+                if kv:
+                    key = kv[0]
+
+                    if key in mapping:
+                        if mapping[key] == kv[1]:
+                            identical += 1
+                        else:
+                            translated += 1
+
+                        f1.write(key + "=" + mapping[key] + "\n")
+                        del mapping[key]
                     else:
-                        translated += 1
-
-                    f1.write(key + "=" + mapping[key] + "\n")
-                    del mapping[key]
+                        untranslated += 1
+                        f1.write("#" + line)
                 else:
-                    untranslated += 1
-                    f1.write("#" + line)
-            else:
-                f1.write(line)
+                    f1.write(line)
 
-        score = round((translated + identical) / entries * 100)
-        print_row(f, str(score) + "%", translated, identical, untranslated, len(mapping))
+            score = (translated + identical) / entries * 100
+            scores.append(score)
 
-        if mapping:
-            f1.write("\n# Unmapped lines\n")
+            data[f] = get_row(silent, f, str(round(score)) + "%", translated, identical, untranslated, len(mapping))
 
-            for key in mapping:
-                line = key + "=" + mapping[key]
-                f1.write(line + "\n")
+            if mapping:
+                f1.write("\n# Unmapped lines\n")
+
+                for key in mapping:
+                    line = key + "=" + mapping[key]
+                    f1.write(line + "\n")
+
+    data["total"] = get_row(silent, "total", str(round(sum(scores) / len(scores))) + "%")
+    return data
+
+if __name__ == "__main__":
+    import sys
+    isjson = "-j" in sys.argv or "--json" in sys.argv
+
+    data = get_data(isjson)
+
+    if isjson:
+        import json
+        print(json.dumps(data))
