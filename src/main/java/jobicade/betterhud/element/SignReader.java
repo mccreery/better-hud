07b86df59d503ba4f41fbdc5e04ca88f617984c3
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-01-18 17:14:35 +0000

    Fix sign reader text shadow

--- src/main/java/jobicade/betterhud/element/SignReader.java
+++ src/main/java/jobicade/betterhud/element/SignReader.java
@@ -2,20 +2,24 @@ package jobicade.betterhud.element;
 
 import static jobicade.betterhud.BetterHud.MC;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import jobicade.betterhud.element.settings.DirectionOptions;
 import jobicade.betterhud.element.settings.SettingPosition;
 import jobicade.betterhud.geom.Rect;
 import jobicade.betterhud.render.Color;
-import jobicade.betterhud.util.GlUtil;
+import jobicade.betterhud.render.Grid;
+import jobicade.betterhud.render.Label;
+import jobicade.betterhud.render.Quad;
 import jobicade.betterhud.geom.Direction;
 import jobicade.betterhud.geom.Point;
 
@@ -33,33 +37,38 @@ public class SignReader extends HudElement {
 	}
 
 	@Override
+	public boolean shouldRender(Event event) {
+		return getSign() != null;
+	}
+
+	@Override
 	public Rect render(Event event) {
-		RayTraceResult trace = MC.getRenderViewEntity().rayTrace(200, 1.0F);
-
-		IBlockState state = MC.world.getBlockState(trace.getBlockPos());
-		if(state == null || !(state.getBlock() instanceof ITileEntityProvider)) {
-			return null;
-		}
-
-		TileEntity tileEntity = MC.world.getTileEntity(trace.getBlockPos());
-		if(tileEntity == null || !(tileEntity instanceof TileEntitySign)) {
-			return null;
-		}
-		TileEntitySign sign = (TileEntitySign)tileEntity;
-
 		Rect bounds = position.applyTo(new Rect(96, 48));
 
 		MC.getTextureManager().bindTexture(SIGN_TEXTURE);
-		Gui.drawScaledCustomSizeModalRect(bounds.getX(), bounds.getY(), 2, 2, 24, 12, 96, 48, 64, 32);
+		new Quad().setTexture(new Rect(2, 2, 24, 12).scale(4, 8)).render(bounds);
 
-		Point pos = bounds.getAnchor(Direction.NORTH).add(0, 3);
-		for(ITextComponent line : sign.signText) {
-			if(line != null) {
-				GlUtil.drawString(line.getFormattedText(), pos, Direction.NORTH, Color.BLACK);
-			}
-			pos = pos.add(0, MC.fontRenderer.FONT_HEIGHT + 2);
-		}
+		List<Label> labels = Stream.of(getSign().signText)
+			.map(line -> new Label(line.getFormattedText()).setColor(Color.BLACK).setShadow(false))
+			.collect(Collectors.toList());
+
+		Grid<Label> grid = new Grid<>(new Point(1, labels.size()), labels);
+		grid.render(bounds.grow(-3));
 
 		return bounds;
+	}
+
+	private TileEntitySign getSign() {
+		RayTraceResult trace = MC.getRenderViewEntity().rayTrace(200, 1.0F);
+		IBlockState state = MC.world.getBlockState(trace.getBlockPos());
+
+		if(state.getBlock() instanceof ITileEntityProvider) {
+			TileEntity tileEntity = MC.world.getTileEntity(trace.getBlockPos());
+
+			if(tileEntity instanceof TileEntitySign) {
+				return (TileEntitySign)tileEntity;
+			}
+		}
+		return null;
 	}
 }
