57e6f80b52292b15004d4673eb42b6ae4a2085a7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-05-29 18:43:00 +0100

    Relax sorter to only require Iterable

--- src/main/java/jobicade/betterhud/util/Sorter.java
+++ src/main/java/jobicade/betterhud/util/Sorter.java
@@ -1,21 +1,19 @@
 package jobicade.betterhud.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Lists;
 
-import java.util.Comparator;
-
 public class Sorter<T> {
-	private final List<T> sourceData;
+	private final Iterable<T> sourceData;
 	private final Map<Comparator<T>, List<T>> sortedDataMap = new HashMap<Comparator<T>, List<T>>();
 
-	public Sorter(List<T> sourceData) {
+	public Sorter(Iterable<T> sourceData) {
 		this.sourceData = sourceData;
 	}
 
@@ -47,7 +45,7 @@ public class Sorter<T> {
 
 	private List<T> ensureEntry(Comparator<T> comparator) {
 		return sortedDataMap.computeIfAbsent(comparator, c -> {
-			List<T> sortedData = new ArrayList<T>(sourceData);
+			List<T> sortedData = Lists.newArrayList(sourceData);
 			sortedData.sort(c);
 
 			return sortedData;
