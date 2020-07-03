c84283c6e098c93252dc7018bcf580a8fe3f4efb
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-07-03 19:50:23 +0100

    Remove sorter

--- src/main/java/jobicade/betterhud/registry/HudRegistry.java
+++ src/main/java/jobicade/betterhud/registry/HudRegistry.java
@@ -1,18 +1,15 @@
 package jobicade.betterhud.registry;
 
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import jobicade.betterhud.BetterHud;
 import jobicade.betterhud.element.HudElement;
-import jobicade.betterhud.util.Sorter;
 
 public abstract class HudRegistry<T extends HudElement<?>> {
     private final HudRegistry<? super T> parentRegistry;
     private final Map<String, T> elements = new HashMap<>();
-    private final Sorter<T> sorter = new Sorter<>(elements.values());
 
     public HudRegistry(HudRegistry<? super T> parentRegistry) {
         this.parentRegistry = parentRegistry;
@@ -40,28 +37,13 @@ public abstract class HudRegistry<T extends HudElement<?>> {
         if (oldElement != null) {
             BetterHud.getLogger().warn("Duplicate registry key \"%s\" overwritten");
         }
-
-        sorter.markDirty();
     }
 
-    public final List<T> getRegistered() {
-        return getRegistered(SortField.ALPHABETICAL);
-    }
-
-    public final List<T> getRegistered(Comparator<? super T> comparator) {
-        return sorter.getSortedData(comparator);
+    public final Collection<T> getRegistered() {
+        return elements.values();
     }
 
     public final T getRegistered(String name) {
         return elements.get(name);
-    }
-
-    @SafeVarargs
-    public final void invalidateSorts(Comparator<?>... comparators) {
-        sorter.markDirty(comparators);
-
-        if (parentRegistry != null) {
-            parentRegistry.invalidateSorts(comparators);
-        }
     }
 }
