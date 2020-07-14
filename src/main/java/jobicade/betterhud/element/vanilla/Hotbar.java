962696258fd791c30730f319b0f0612dfe8ac291
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-07-14 11:47:09 +0100

    Fix hotbar spectator mode menu

--- src/main/java/jobicade/betterhud/element/vanilla/Hotbar.java
+++ src/main/java/jobicade/betterhud/element/vanilla/Hotbar.java
@@ -11,6 +11,8 @@ import jobicade.betterhud.util.GlUtil;
 import jobicade.betterhud.util.Textures;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiSpectator;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 
@@ -29,12 +31,34 @@ public class Hotbar extends OverlayElement {
 
     @Override
     public boolean shouldRender(OverlayContext context) {
+        final Minecraft mc = Minecraft.getMinecraft();
+
         return GuiIngameForge.renderHotbar
-            && !OverlayHook.pre(context.getEvent(), ElementType.HOTBAR);
+            && !OverlayHook.pre(context.getEvent(), ElementType.HOTBAR)
+            && (
+                !mc.playerController.isSpectator()
+                || mc.ingameGUI.getSpectatorGui().isMenuActive()
+            );
     }
 
     @Override
     public Rect render(OverlayContext context) {
+        final Minecraft mc = Minecraft.getMinecraft();
+
+        if (Minecraft.getMinecraft().playerController.isSpectator()) {
+            GuiSpectator spectator = mc.ingameGUI.getSpectatorGui();
+            ScaledResolution res = new ScaledResolution(mc);
+
+            spectator.renderTooltip(res, context.getPartialTicks());
+            spectator.renderSelectedItem(res);
+
+            return new Rect(182, 22).anchor(context.getLayoutManager().getScreen(), Direction.SOUTH);
+        } else {
+            return renderHotbar(context, mc);
+        }
+    }
+
+    private Rect renderHotbar(OverlayContext context, Minecraft mc) {
         Rect barTexture = new Rect(182, 22);
         Rect bounds = position.applyTo(new Rect(barTexture));
 
