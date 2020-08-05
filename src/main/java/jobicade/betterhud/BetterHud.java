63d06468a714929d8b545f8387c96ee79af83535
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-05 19:31:34 +0100

    Use new setup for main mod class

--- src/main/java/jobicade/betterhud/BetterHud.java
+++ src/main/java/jobicade/betterhud/BetterHud.java
@@ -1,6 +1,9 @@
 package jobicade.betterhud;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 import jobicade.betterhud.geom.LayoutManager;
 import jobicade.betterhud.network.InventoryNameQuery;
@@ -10,33 +13,27 @@ import jobicade.betterhud.network.MessagePickupHandler;
 import jobicade.betterhud.network.MessageVersion;
 import jobicade.betterhud.proxy.HudSidedProxy;
 import jobicade.betterhud.util.Tickable.Ticker;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.common.versioning.ArtifactVersion;
-import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 @EventBusSubscriber
-@Mod(modid = BetterHud.MODID, name = "Better HUD", version = BetterHud.VERSION,
-    updateJSON = "https://raw.githubusercontent.com/mccreery/better-hud/master/update.json",
-    dependencies = "required-after:forge@[14.23.1.2557,)")
+@Mod(BetterHud.MODID)
 public class BetterHud {
     public static final String MODID = "betterhud";
-    public static final String VERSION = "1.4.2";
+    public static final String VERSION = "1.5-alpha";
 
     private static ArtifactVersion serverVersion;
 
     public BetterHud() {
-        setServerVersion(null);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
-    private static Logger logger;
+    private static final Logger logger = LogManager.getLogger();
     public static Logger getLogger() {
         return logger;
     }
@@ -54,16 +51,18 @@ public class BetterHud {
 
     public static final int SPACER = 5;
 
-    public static final SimpleNetworkWrapper NET_WRAPPER = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel NET_WRAPPER = NetworkRegistry.newSimpleChannel(
+        new ResourceLocation(MODID, "main"),
+        () -> PROTOCOL_VERSION,
+        PROTOCOL_VERSION::equals,
+        PROTOCOL_VERSION::equals
+    );
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
+    private void setup(FMLCommonSetupEvent event) {
+        setServerVersion(null);
         proxy.preInit(event);
-    }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
         // Message ID 0 reserved for ignored server presence message from [,1.4)
         NET_WRAPPER.registerMessage(MessagePickupHandler.class, MessagePickup.class, 1, Side.CLIENT);
         NET_WRAPPER.registerMessage(MessageNotifyClientHandler.class, MessageVersion.class, 2, Side.CLIENT);
