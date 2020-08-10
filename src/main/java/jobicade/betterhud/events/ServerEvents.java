1db774ac30087a0b33df7fd5577a9a1b427ac809
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-10 14:29:38 +0100

    Fix ServerEvents errors

--- src/main/java/jobicade/betterhud/events/ServerEvents.java
+++ src/main/java/jobicade/betterhud/events/ServerEvents.java
@@ -1,19 +1,22 @@
 package jobicade.betterhud.events;
 
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+
 import jobicade.betterhud.BetterHud;
 import jobicade.betterhud.network.InventoryNameQuery;
 import jobicade.betterhud.network.MessagePickup;
 import jobicade.betterhud.network.MessageVersion;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.world.IWorldNameable;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.INameable;
+import net.minecraftforge.event.entity.player.PlayerEvent.ItemPickupEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
-import net.minecraftforge.fml.common.versioning.ArtifactVersion;
-import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
+import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.fml.network.PacketDistributor.PacketTarget;
 
 @EventBusSubscriber(modid = BetterHud.MODID)
 public class ServerEvents {
@@ -23,9 +26,11 @@ public class ServerEvents {
      */
     @SubscribeEvent
     public static void onPlayerConnected(PlayerLoggedInEvent e) {
-        if(e.player instanceof EntityPlayerMP) {
+        if(e.getPlayer() instanceof ServerPlayerEntity) {
             ArtifactVersion version = new DefaultArtifactVersion(BetterHud.VERSION);
-            BetterHud.NET_WRAPPER.sendTo(new MessageVersion(version), (EntityPlayerMP)e.player);
+
+            PacketTarget target = PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)e.getPlayer());
+            BetterHud.NET_WRAPPER.send(target, new MessageVersion(version));
         }
     }
 
@@ -34,11 +39,9 @@ public class ServerEvents {
      */
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-        if (event.getWorld().getTileEntity(event.getPos()) instanceof IWorldNameable) {
-            BetterHud.NET_WRAPPER.sendToDimension(
-                new InventoryNameQuery.Response(event.getPos(), null),
-                event.getWorld().provider.getDimension()
-            );
+        if (event.getWorld().getTileEntity(event.getPos()) instanceof INameable) {
+            PacketTarget target = PacketDistributor.DIMENSION.with(event.getWorld().getDimension()::getType);
+            BetterHud.NET_WRAPPER.send(target, new InventoryNameQuery.Response(event.getPos(), null));
         }
     }
 
@@ -47,11 +50,9 @@ public class ServerEvents {
      */
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onItemPickup(ItemPickupEvent e) {
-        if (!e.getStack().isEmpty() && e.player instanceof EntityPlayerMP) {
-            BetterHud.NET_WRAPPER.sendTo(
-                new MessagePickup(e.getStack()),
-                (EntityPlayerMP)e.player
-            );
+        if (!e.getStack().isEmpty() && e.getPlayer() instanceof ServerPlayerEntity) {
+            PacketTarget target = PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)e.getPlayer());
+            BetterHud.NET_WRAPPER.send(target, new MessagePickup(e.getStack()));
         }
     }
 }
