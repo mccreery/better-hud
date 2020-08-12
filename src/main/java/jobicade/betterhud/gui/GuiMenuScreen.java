e5e6edbb70bff7a21b9bc71f4653b96322a5d7ee
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-12 21:31:15 +0100

    Simplify menu screen

--- src/main/java/jobicade/betterhud/gui/GuiMenuScreen.java
+++ src/main/java/jobicade/betterhud/gui/GuiMenuScreen.java
@@ -4,43 +4,21 @@ import jobicade.betterhud.geom.Direction;
 import jobicade.betterhud.geom.Point;
 import jobicade.betterhud.render.Color;
 import jobicade.betterhud.util.GlUtil;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.text.ITextComponent;
 
 public class GuiMenuScreen extends Screen {
-    private Point origin;
-    private String title;
-
     public GuiMenuScreen(ITextComponent title) {
         super(title);
     }
 
-    @Override
-    protected void actionPerformed(Button button) {
-        if(button instanceof GuiActionButton) {
-            ((GuiActionButton)button).actionPerformed();
-        }
-    }
-
-    @Override
-    public void resize(Minecraft mc, int width, int height) {
-        this.origin = new Point(width / 2, height / 16 + 20);
-        super.resize(mc, width, height);
-    }
-
     protected Point getOrigin() {
-        return origin;
-    }
-
-    protected void setTitle(String title) {
-        this.title = title;
+        return new Point(width / 2, height / 16 + 20);
     }
 
     protected void drawTitle() {
         if(title != null) {
-            GlUtil.drawString(title, origin.sub(0, 15), Direction.NORTH, Color.WHITE);
+            GlUtil.drawString(title.getFormattedText(), getOrigin().sub(0, 15), Direction.NORTH, Color.WHITE);
         }
     }
 
