fa9d055b04e02cd0af61e94859f4a8a241fd90b2
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-08-23 18:28:44 +0100

    Fix biome name text

--- src/main/java/jobicade/betterhud/element/text/BiomeName.java
+++ src/main/java/jobicade/betterhud/element/text/BiomeName.java
@@ -16,6 +16,6 @@ public class BiomeName extends TextElement {
     protected List<String> getText() {
         BlockPos pos = new BlockPos((int)MC.player.getPosX(), 0, (int)MC.player.getPosZ());
 
-        return Arrays.asList(getLocalizedName() + ": " + MC.world.getBiome(pos).getDisplayName());
+        return Arrays.asList(getLocalizedName() + ": " + MC.world.getBiome(pos).getDisplayName().getFormattedText());
     }
 }
